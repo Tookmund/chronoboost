94cc8c80b65bea25893e0fec9666b5c04bc455a1
Toukmond <Toukmond@users.noreply.github.com>
2014-05-01 15:35:05 -0400

    rename to code.ino and update stuff
    comments
    prevent looping
    change const to vars
    Serial for debugging

--- code.ino
+++ code.ino
@@ -1,30 +1,46 @@
-
-const int left = 10;
-const int right = 11;
-const int torso = 9;
+int left = 10;
+int right = 11;
+int torso = 9;
 
 void setup () {
   pinMode(left, OUTPUT);
   pinMode(right, OUTPUT);
   pinMode(torso, OUTPUT);
+  Serial.begin(2400);
 }
 
 void loop () {
+  //wait 5 seconds after being turned on
+  Serial.print("Waiting...");
+  delay(5000);
+  //move left wheel then right wheel 
   for (int i = 0; i < 12; i++) {
+    Serial.println("left");
     digitalWrite(left, HIGH);
     delay(5000);
     digitalWrite(left, LOW);
+    
+    Serial.println("right");
     digitalWrite(right, HIGH);
     delay(5000);
     digitalWrite(right, LOW);
   }
-  
+  //move forward and then rotate torso
+  Serial.println("forward");
   digitalWrite(left, HIGH);
   digitalWrite(right, HIGH);
   delay(1000);
   digitalWrite(left, LOW);
   digitalWrite(right, LOW);
+  
+  Serial.println("torso");
   digitalWrite(torso, HIGH);
   delay(2000);
   digitalWrite(torso, LOW);
+  
+  //prevent looping
+  left = 2;
+  right = 2;
+  torso = 2;
+  Serial.println("done");
 }
