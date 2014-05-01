485e81ee8f393f7e6ced2f6eb1c85865cb43f504
Toukmond <Toukmond@users.noreply.github.com>
2014-05-01 14:44:58 -0400

    On Off on off
    Start motor1, delay, Stop motor1, delay, Start motor2, delay, Stop motor2

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
 void setup () {
-
+  const int motor1 = 13;
+  const int motor2 = 14;
+  pinMode(motor1, OUTPUT);
+  pinMOde(motor2, OUTPUT);
 }
 
 void loop () {
-
+  digitalWrite(motor1, HIGH);
+  delay(1000);
+  digitalWrite(motor1, LOW);
+  digitalWrite(motor2, HIGH);
+  delay(1000);
+  digitalWrite(motor2, LOW);
 }
