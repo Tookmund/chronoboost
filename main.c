4d6d306c54f32931fe52c33d50c5c2fea1fad3b4
Toukmond <Toukmond@users.noreply.github.com>
2014-05-01 15:02:00 -0400

    finished skeleton code prior to testing
    finished skeleton code

--- main.c
+++ main.c
@@ -1,15 +1,27 @@
 void setup () {
-  const int motor1 = 13;
-  const int motor2 = 14;
+  const int left = 10;
+  const int right = 11;
+  const int torso = 9;
   pinMode(motor1, OUTPUT);
   pinMOde(motor2, OUTPUT);
 }
 
 void loop () {
-  digitalWrite(motor1, HIGH);
+  for (int i = 0; i < 12; i++) {
+    digitalWrite(left, HIGH);
+    delay(5000);
+    digitalWrite(left, LOW);
+    digitalWrite(right, HIGH);
+    delay(5000);
+    digitalWrite(right, LOW);
+  }
+  
+  digitalWrite(left, HIGH);
+  digitalWrite(right, HIGH);
   delay(1000);
-  digitalWrite(motor1, LOW);
-  digitalWrite(motor2, HIGH);
-  delay(1000);
-  digitalWrite(motor2, LOW);
+  digitalWrite(left, LOW);
+  digitalWrite(right, LOW);
+  digitalWrite(torso, HIGH);
+  delay(2000);
+  digitalWrite(torso, LOW);
 }
